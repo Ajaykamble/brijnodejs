81b561db8f6cd7abd556ba253f47adbbc252e4e4
Ajay Kamble <ajay.kamble119@gmail.com>
2024-06-19 19:46:30 +0530

    updatec chat.js

--- chat.js
+++ chat.js
@@ -1,4 +1,3 @@
-
 var buffer = require('buffer');
 var path = require('path');
 var fs = require('fs');
@@ -6,8 +5,9 @@ const http = require('http');
 const printer = require('@thiagoelg/node-printer');
 const { print, getPrinters, getDefaultPrinter } = require('pdf-to-printer');
 const request = require('request');
-var exec = require('child_process').exec;
+var { spawn } = require('child_process');
 const axios = require('axios');
+const { v4: uuidv4 } = require('uuid'); // Using UUID for unique filenames
 
 module.exports = function (io) {
     console.log("CA");
@@ -23,36 +23,20 @@ module.exports = function (io) {
                 var printTo = myArray[2];
                 var pType = myArray[3];
                 var sendPrint = myArray[4];
-/*
-Connected 2024-03-19 18:43:26*24*Restaurant*KOT*Restaurant
-Connected http://192.168.0.60/brij/print_pos.php?id=24&type=Restaurant&main=Restaurant
-PDFtoPrinter.exe invoice_20240319131329758Z.pdf "RESTKITCHEN"
-Connected 2024-03-19 18:43:26*24*Restaurant*KOT*Fastfood
-Connected http://192.168.0.60/brij/print_pos.php?id=24&type=Restaurant&main=Fastfood
-PDFtoPrinter.exe invoice_20240319131329852Z.pdf "FASTFOOD"
-*/
-//console.log("Connected", sendPrint + "Hello Rupesh");
-                //const phpUrl = 'http://192.168.0.60/brij/print_pos.php?id='+invoiceId+'&type='+printTo;
+
                 var phpUrl = "";
                 if (pType == "KOT") {
                     phpUrl = encodeURI('http://192.168.0.60/brij/print_pos.php?id=' + invoiceId + '&type=' + printTo + '&main=' + sendPrint);
-                }
-                else if (pType == "KOTDEL") {
+                } else if (pType == "KOTDEL") {
                     phpUrl = encodeURI('http://192.168.0.60/brij/print_pos_del.php?id=' + invoiceId + '&type=' + printTo + '&main=' + sendPrint + '&rowno=' + date);
-                }
-                else if (pType == "PrintDS") {
+                } else if (pType == "PrintDS") {
                     phpUrl = encodeURI('http://192.168.0.60/brij/printdetsalesummary.php?a=' + date + '&b=' + invoiceId);
-                }
-                else {
+                } else {
                     if (sendPrint == "Restaurant") {
                         phpUrl = encodeURI('http://192.168.0.60/brij/print_pos_rest.php?id=' + invoiceId + '&type=' + printTo + '&main=' + sendPrint);
-                
-                    }
-                    else
-                    {
+                    } else {
                         phpUrl = encodeURI('http://192.168.0.60/brij/print_pos_fast_bill.php?id=' + invoiceId + '&type=' + printTo + '&main=' + sendPrint);
                     }
-                    
                 }
                 console.log("Connected", phpUrl);
                 const response = await axios.post(phpUrl, {}, {
@@ -60,67 +44,52 @@ PDFtoPrinter.exe invoice_20240319131329852Z.pdf "FASTFOOD"
                 });
                 const formattedDate = new Date().toISOString().replace(/[-:.T]/g, '');
 
-                var filename = "invoice_" + formattedDate + ".pdf";
+                var filename = `invoice_${formattedDate}_${uuidv4()}.pdf`;
                 const writableStream = fs.createWriteStream(filename);
                 response.data.pipe(writableStream);
                 console.log(sendPrint);
                 writableStream.on('finish', async () => {
                     var printerName = "";
-                    if (pType == "KOT" || pType == "KOTDEL") 
-                    {
+                    if (pType == "KOT" || pType == "KOTDEL") {
                         if (sendPrint == "Restaurant") {
-                            //printerName = "CHAI"; 
-                            printerName  = "RESTKITCHEN"; 
-                             //printerName  = "RESTPOS";
-                             //printerName  = "MOTELPOS";
-                             //printerName = "FASTFOOD"; 
-                            // printerName = "POSFASTFOOD"; 
+                            printerName = "RESTKITCHEN";
+                        } else {
+                            printerName = "FASTFOOD";
                         }
-                        else {
-                           // printerName = "CHAI"; 
-                            printerName = "FASTFOOD"; 
-                          //printerName  = "RESTPOS";
-                        }
-                    }
-                    else if (pType == "PrintDS") 
-                        {
-                            if (sendPrint == "Restaurant") {
-
-                               //printerName  = "RESTKITCHEN"; 
-                                 //printerName  = "RESTPOS";
-                                 //printerName  = "MOTELPOS";
-                                 //printerName = "FASTFOOD"; 
-                                 printerName = "CHAI"; 
-                            }
-                            else {
-                                //printerName = "FASTFOOD"; 
-                              //printerName  = "RESTPOS";
-                              printerName = "CHAI"; 
-                            }
-                        }
-                    else
-                    {
+                    } else if (pType == "PrintDS") {
                         if (sendPrint == "Restaurant") {
-                           // printerName = "CHAI"; 
+                            printerName = "CHAI";
+                        } else {
+                            printerName = "CHAI";
+                        }
+                    } else {
+                        if (sendPrint == "Restaurant") {
                             printerName = "RESTPOS";
-                        }
-                        else {
-                           // printerName = "CHAI"; 
-                            
-                            printerName = "POSFASTFOOD"; 
+                        } else {
+                            printerName = "POSFASTFOOD";
                         }
                     }
-                    var cmd = 'PDFtoPrinter.exe ' + filename + ' "' + printerName + '"';
-                    console.log(cmd);
-                    exec(cmd, function (error, stdout, stderr) {
-                        // command output is in stdout
-
+                    var cmd = 'PDFtoPrinter.exe';
+                    var args = [filename, printerName];
+                    console.log(`${cmd} ${args.join(' ')}`);
+                    
+                    const child = spawn(cmd, args);
+                    child.on('close', (code) => {
+                        if (code === 0) {
+                            console.log(`Printed ${filename} successfully`);
+                            fs.unlink(filename, (err) => {
+                                if (err) {
+                                    console.error(`Error deleting file: ${err}`);
+                                } else {
+                                    console.log(`Deleted file: ${filename}`);
+                                }
+                            });
+                        } else {
+                            console.error(`Printing failed with code ${code}`);
+                        }
                     });
                 });
             }
-
-
         });
     });
 };
-
